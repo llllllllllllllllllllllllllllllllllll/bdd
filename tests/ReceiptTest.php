736ff9444c333e2d3b70d23747c7fddd598e82fb
aleksander <aleksander.promvalds@khk.ee>
2019-05-27 05:02:08 +0300

    ReceiptTest

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -18,6 +18,8 @@ class ReceiptTest extends TestCase {
 	 * @dataProvider provideTotal
 	 */
 	public function testTotal($items, $expected) {
+	    // Kontrollib, et Receipt klassi total funktsioon tagastaks provideTotal poolt antavatele andmetele vastavad tulemused.
+        // Kupongi väärtus selle testi puhul on alati null, seega kupongi toimivust ei arvestata.
 		$coupon = null;
 		$output = $this->Receipt->total($items, $coupon);
 		$this->assertEquals(
@@ -35,6 +37,8 @@ class ReceiptTest extends TestCase {
 		];
 	}
 	public function testTotalAndCoupon() {
+	    // Testib total funktsiooni toimivust, kasutades kupongi. Seekord on esemete väärtused antud funktsiooni sees $input muutujana.
+        // Antud esemete summa on 15 ja kupong peaks summat 20% võrra vähendama, seega oodatav tulemus on 12.
 		$input = [0,2,5,8];
 		$coupon = 0.20;
 		$output = $this->Receipt->total($input, $coupon);
@@ -46,6 +50,7 @@ class ReceiptTest extends TestCase {
 	}
 
 	public function testTotalException() {
+	    // Testib, et total funktsioon ei laseks kasutada kupongi, mis vähendab esemete summat rohkem kui 100% võrra.
 		$input = [0,2,5,8];
 		$coupon = 1.20;
 		$this->expectException('BadMethodCallException');
@@ -56,6 +61,7 @@ class ReceiptTest extends TestCase {
 		$items = [1,2,5,8];
 		$tax = 0.20;
 		$coupon = null;
+		// Testib total ja tax funktsioone mocki kasutades.
 		$Receipt = $this->getMockBuilder('TDD\Receipt')
 			->setMethods(['tax', 'total'])
 			->getMock();
@@ -67,12 +73,13 @@ class ReceiptTest extends TestCase {
 			->method('tax')
 			->with(10.00, $tax)
 			->will($this->returnValue(1.00));
+		// Testib postTaxTotal funktsiooni toimivust.
 		$result = $Receipt->postTaxTotal([1,2,5,8], 0.20, null);
 		$this->assertEquals(11.00, $result);
 	}
 
 	public function testTax() {
-
+        // Testib, et tax funktsioon tagastaks inputAmounti ja taxInputi summa.
 		$inputAmount = 10.00;
 		$taxInput = 0.10;
 		$output = $this->Receipt->tax($inputAmount, $taxInput);
